00e3efdc8703d3bef37697dab729bd3dc5961037
Santiago <Santiago@Dayscript>
2014-06-26 21:11:11 -0500

    Preparando el Formulario de Crear Curso

--- application/views/administrador/contenido/inferior/vw_crear_curso.php
+++ application/views/administrador/contenido/inferior/vw_crear_curso.php
@@ -4,22 +4,26 @@
     });
 
 </script>
-<div class="titulo_modal" style="text-align: center">
-    <span class="borde_izq_boton_agregar" >&nbsp;</span>
-    <span class="borde_centro_boton_agregar">
-        Curso Nuevo
+<div class="titulo_modal">
+    <span>
+        <img src="<?php echo HTTP_IMAGES_PATH?>Administrador/Contenido/border_izq_btn_rojo.png" />
+        <span class="titulo_modal_cuerpo">
+            Curso Nuevo
+        </span>
+        <img class="titulo_modal_imagen_izq" src="<?php echo HTTP_IMAGES_PATH?>Administrador/Contenido/border_der_btn_rojo.png" />
     </span>
-    <span class="borde_der_boton_agregar">&nbsp;</span>
 </div>
 <div class="cuerpo_modal">
     <?php
-
         echo validation_errors();
 
         $attributes = array('id' => 'f_nuevo_curso', 'name' => 'f_nuevo_curso');
         //$hidden = array('tipo' => 'U');
         echo form_open('cursos/crear_curso', $attributes);
-
+    ?>
+    
+    <div class="form_modal_contenido">
+    <?php    
         $data = array('name'=>'nombre', 'id'=>'nombre', 'value'=>set_value("nombre"), 'maxlength'=>'100', 'style'=>'width:50%', 'type'=>'text');
         echo "Nombres:".form_input($data)."<br/>";
 
@@ -28,7 +32,10 @@
 
         $data = array('name'=>'correo', 'id'=>'correo', 'value'=>set_value("correo"), 'maxlength'=>'100', 'style'=>'width:50%', 'type'=>'text');
         echo "Correo:".form_input($data)."<br/>";
-
+    ?>
+    </div>
+    <div class="form_modal_contenido">
+    <?php  
         $data = array('name'=>'cedula', 'id'=>'cedula', 'value'=>set_value("cedula"), 'maxlength'=>'100', 'style'=>'width:50%', 'type'=>'text');
         echo "C&eacute;dula:".form_input($data)."<br/>";
 
@@ -37,7 +44,7 @@
 
         $data = array('name'=>'contrasena', 'id'=>'contrasena', 'value'=>set_value("contrasena"), 'maxlength'=>'100', 'style'=>'width:50%', 'type'=>'password');
         echo "Contrase&ntilde;a:".form_input($data)."<br/>";
-
+        
         /*$options = array('S'=>'Super Administrador', 'V'=>'Vendedores', 'U'=>'Asistente (Administrador)');
         echo "Tipo:".form_dropdown('tipo', $options, 'U')."<br/>";*/
 
@@ -45,5 +52,6 @@
 
         echo form_close();
     ?>
+    </div>
 </div>
 <div class="boton_modal"></div>
